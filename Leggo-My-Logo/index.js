0c858c1f66b0a4100babf01b21c9b3c6a3e7fc67
victormora1 <victorjmora1@gmail.com>
2023-07-13 22:47:32 -0400

    changed the folder name in shape.js, and chznged the name as well in setshape.js

--- Leggo-My-Logo/index.js
+++ Leggo-My-Logo/index.js
@@ -1,9 +1,9 @@
 //import inquirer
 const inquirer = require('inquirer');
 const fs = require('fs');
-const questions = require('./lib/questions.js');
+const questions = require('../lib/questions');
 const fileName = './examples/logo.svg';
-const setshape = require('./lib/shapes.js');
+const setshape = require('../lib/shape');
 
 function createLogo(response) {
     const svg = setShape(response);
